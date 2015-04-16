de1705affb9eddd43797ff9ac4ff06a6ed361cdb
losserev <losserev@gmail.com>
2015-04-16 00:01:11 -0400

    Posedge detector to deal with 'several posedges in the always block' error

---
+++ PosedgeDetector.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+// Posedge detector to deal with 'several posedges in the always block' error
+
+module PosedgeDetector(edgeout, clk, signal);
+
+	input clk;
+	input signal;
+	
+	output edgeout;
+	
+	wire signal;
+	wire edgeout;
+	reg signal_d;
+ 
+	always @ (posedge clk) begin
+		signal_d <= signal;
+	end
+	
+	assign edgeout = signal & (~signal_d);
+
+endmodule
